a49a2c5b4c49959d1567118a07c4575651960144
Trifa Andreea <andreea_trifa@yahoo.com>
2021-10-23 15:10:31 +0300

    Adaugare tema 3

---
+++ Trifa-Andreea/L03/Program.cs
@@ -0,0 +1,89 @@
+﻿using Newtonsoft.Json.Linq;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Drive.v3;
+using Google.Apis.Drive.v3.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using Google.Apis.Requests;
+using System;
+using System.Net;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace L03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Initialize();
+        }
+
+        private static DriveService service_;
+        private static string token_;
+
+        static void Initialize()
+        {
+            string[] scopes = new string[] {
+                DriveService.Scope.Drive,
+                DriveService.Scope.DriveFile
+            };
+
+            var client_id = "735192804404-lmbcldpgtfcn77ir3bl6kegiius74erq.apps.googleusercontent.com";
+            var client_secret = "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";
+
+            var credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                new ClientSecrets
+                {
+                    ClientId = client_id,
+                    ClientSecret = client_secret
+                },
+                scopes,
+                Environment.UserName,
+                CancellationToken.None,
+
+                new FileDataStore("Daimto.GoogleDrive.Auth.Store3")
+            ).Result;
+
+            service_ = new DriveService( new Google.Apis.Services.BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential
+            });
+
+            token_ = credential.Token.AccessToken;
+
+            Console.Write("Token: " + credential.Token.AccessToken);
+
+            GetMyFiles();
+        }
+
+        static void GetMyFiles()
+        {
+            var request = (HttpWebRequest)WebRequest.Create("https://googleapis.com/drive/v3/files?q='root'%20in%20parents");
+            request.Headers.Add(HttpRequestHeader.Authorization, "Bearer " + token_);
+            request.Method = WebRequestMethods.Http.Post;
+
+            using (var response = request.GetResponse())
+            {
+                using (Stream data = response.GetResponseStream())
+                using (var reader = new StreamReader(data))
+                {
+                    string text = reader.ReadToEnd();
+                    var mydata = JObject.Parse(text);
+                    foreach(var file in mydata["files"])
+                    {
+                        if (file["mimeType"].ToString() != "application/vnd.google-apps.folder")
+                        {
+                            Console.WriteLine("File name: " + file["name"]);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
